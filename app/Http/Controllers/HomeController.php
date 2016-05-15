5d730f9ea874957138a2a6415069553b81e79f8a
Bruno Pereira <bruno9pereira@gmail.com>
2016-05-15 10:46:06 -0300

    initial commit"

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class HomeController extends Controller
+{
+    const API_ROOT_URL = 'http://localhost:8000/';
+
+
+    public function home(string $name)
+    {
+        return view('home')->with('data', [
+            'feed' => $this->getContentFrom(self::API_ROOT_URL . "feed/$name"),
+            'episodes' => $this->getContentFrom(self::API_ROOT_URL . "episodes/$name")
+        ]);
+    }
+
+    private function getContentFrom($source)
+    {
+        $curl = curl_init($source);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        return json_decode(curl_exec($curl), true);
+    }
+}
