58dba809a8f2f83864c5309ebfff69b44aa375b5
brnbp <bruno9pereira@gmail.com>
2016-06-12 04:11:18 -0300

    customize player to keep in bottom page

--- public/js/podcast.js
+++ public/js/podcast.js
@@ -1,1 +1,22 @@
-$(() => $( 'audio' ).audioPlayer());
+'use strict';
+
+const getAudioTag = (mediaUrl, mediaType) => '<audio controls src="' + mediaUrl + '" type="' + mediaType + ' "></audio>';
+
+const removeChildren = (e) => e.children().remove();
+
+const playButton = $('.play');
+const playerFooter = $('.audioplayer-footer');
+
+playButton.click(function() {
+    const mediaUrl = $(this).find('#url').val();
+    const mediaType = $(this).find('#type').val();
+
+    removeChildren(playerFooter);
+
+    playerFooter
+        .attr('hidden', false)
+        .append(getAudioTag(mediaUrl, mediaType));
+
+    $(() => $('audio').audioPlayer());
+});
+
