b678b6ace57bf47ce01f20e211ffd41671615873
brnbp <bruno9pereira@gmail.com>
2016-06-14 02:58:33 -0300

    add search by episode

--- public/js/podcast.js
+++ public/js/podcast.js
@@ -6,7 +6,6 @@ const removeChildren = (e) => e.children().remove();
 
 const playButton = $('.play');
 const playerFooter = $('.audioplayer-footer');
-
 playButton.click(function() {
     const mediaUrl = $(this).find('#url').val();
     const mediaType = $(this).find('#type').val();
@@ -28,3 +27,63 @@ $(document).ready(function($) {
         $(this).toggleClass('share-expanded');
     });
 });
+
+
+
+const addPlaceholder = (e, p = '') => $(e).attr('placeholder', p);
+const placeholderDefault = 'Search for episode';
+
+const inputFindEpisodes = $('.find-episode-search');
+
+inputFindEpisodes.val('');
+addPlaceholder(inputFindEpisodes, placeholderDefault);
+inputFindEpisodes
+    .click(function() { addPlaceholder(this)})
+    .blur(function() { addPlaceholder(this, placeholderDefault) });
+
+const findEpisodesResults = $('.podcasts-episodes');
+const handleViewRender = (response) => {
+    response.forEach((param) => {
+        let content = fulfillInputResult(param)
+        findEpisodesResults.append(content);
+    });
+};
+
+inputFindEpisodes.keypress((e) => {
+    const KEYBOARD_KEY_ENTER = 13;
+    if(e.which != KEYBOARD_KEY_ENTER) {
+        return;
+    }
+    removeChildren(findEpisodesResults);
+    const searchInput = e.currentTarget.value.trim();
+    const podcastId = $('#podcast-id').val();
+
+    if (!searchInput || !podcastId) {
+        return;
+    }
+    
+    $.ajax({
+        type: 'GET',
+        url: 'http://brnpodapi-env.us-east-1.elasticbeanstalk.com/v1/episodes/feedId/' + podcastId + '?term=' + searchInput,
+        success: response => handleViewRender(response)
+    })
+});
+
+
+const fulfillInputResult = function(response) {
+    const header = "<div class='wrapper col-lg-3 col-md-4 col-sm-6 col-xs-12'><div class='card radius shadowDepth1'><div class='card__content card__padding'><div class='card__share'><div class='card__social'><a class='share-icon facebook' href='#'><span class='fa fa-facebook'></span></a><a class='share-icon twitter' href='#'><span class='fa fa-twitter'></span></a><a class='share-icon googleplus' href='#'><span class='fa fa-google-plus'></span></a></div><a id='share' class='share-toggle share-icon' href='#'></a></div><article class='card__article'><h2>";
+    const link = "<a href='" + response.link+ "' target='_blank'>" + response.title + '</a>';
+    const publishedAt = '<br><small>' + formatDateTime(response.published_date) + '</small></h2>';
+    const content = '<p>' + response.content + '</p>';
+    const middle = "</article></div><div class='card__action'><div class='card__author play'><a class='btn center'><i class='fa fa-play-circle fa-2x play' aria-hidden='true'></i>Play</a>";
+    const mediaUrl = "<input type='text' value='" + response.media_url + "' id='url' hidden>";
+    const mediaType = "<input type='text' value='" + response.media_type + "' id='type' hidden>";
+    const footer = '</div></div></div></div>';
+    return header + link + publishedAt + content + middle + mediaUrl + mediaType + footer;
+}
+
+const formatDateTime = function(string){
+    const a = string.split('-');
+    const b = a[2].split(' ');
+    return b[0] + '/' + a[1] + '/' + a[0] + ' ' + b[1];
+};
--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -10,8 +10,8 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-12 col-md-12">
-            <div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="col-lg-9  col-md-8 col-sm-8 col-xs-12">
                 <h4>
                     Latest Episodes of <br><br>
                         <img src="{{ $data['feed']['thumbnail_100'] }}" alt="" class="img-circle">
@@ -19,8 +19,21 @@
                     ({{ $data['feed']['total_episodes'] }})
                 </h4>
             </div>
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <div class="panel panel-default find">
+                    <div class="panel-body find-episode">
+                        <input type="text" class="find-episode-search form-control">
+                        <input type="number" id="podcast-id" hidden value="{{$data['feed']['id']}}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-lg-12 col-md-12  col-sm-12 col-xs-12 text-right podcasts-episodes">
             @foreach ($data['episodes'] as $episode)
-                <div class="wrapper col-lg-4 col-md-6">
+                <div class="wrapper col-lg-3 col-md-4 col-sm-6 col-xs-12">
                     <div class="card radius shadowDepth1">
                         <div class="card__content card__padding">
                             <div class="card__share">
