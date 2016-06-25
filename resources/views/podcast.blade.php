cfc5765f00764be45452d5178bb755546832b1a9
Bruno Pereira <bruno9pereira@gmail.com>
2016-06-24 21:09:49 -0300

    Update podcast.blade.php

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -10,7 +10,7 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="col-lg-9  col-md-8 col-sm-8 col-xs-12">
