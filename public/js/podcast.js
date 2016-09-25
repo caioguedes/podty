2bedaffe89adfec4731a407745542a8d210a2299
brnbp <bruno9pereira@gmail.com>
2016-09-25 02:59:31 -0300

    delete unused assets

--- public/js/podcast.js
+++ public/js/podcast.js
@@ -102,6 +102,17 @@ var getCurrentUrlId = function getCurrentUrlId() {
 };
 
 
+$(document).ready(function(){
+    $(document).on('click', '.play-me', function(){
+        var audio = document.getElementById('player');
+        var source = document.getElementById('source');
+        source.src = $(this).find('input').val();
+        $('#playing span').text($(this).find('input').attr('data-value'));
+        audio.load();
+        $('#audio').attr('hidden', false);
+        audio.play();
+    });
+});
 
 
 
