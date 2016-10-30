43cf580ca06e3e366bae4d9fc61d2dddff05409a
brnbp <bruno9pereira@gmail.com>
2016-10-30 03:27:45 -0200

    update api home podcasts call

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,13 +14,10 @@ class HomeController extends Controller
 {
     use Format;
 
-    private $apiClient;
-
     private $userPodcasts;
 
-    public function __construct(ApiClient $apiClient, UserPodcasts $userPodcasts)
+    public function __construct(UserPodcasts $userPodcasts)
     {
-        $this->apiClient = $apiClient;
         $this->userPodcasts = $userPodcasts;
     }
 
@@ -35,9 +32,9 @@ class HomeController extends Controller
             return redirect('ajax/homeNoFeeds');
         }
 
-        $data = $this->getContentFrom(env('API_BASE_URL') . 'users/' . Auth::user()->name . '/feeds');
+        $response = $this->userPodcasts->all(Auth::user()->name);
 
-        $content = array_map(function($feed){
+        $content = $response->map(function($feed){
             return [
                 "id" => $feed['id'],
                 "name" => $this->formatPodcastName($feed['name']),
@@ -47,33 +44,11 @@ class HomeController extends Controller
                 "listen_all" => $feed['listen_all'],
                 "last_episode_at" => $this->formatData($feed['last_episode_at'])
             ];
-        }, $data);
+        });
 
         return [
             'content' => $content,
             'type' => 'feeds'
         ];
-    }
-    
-    private function getContentFrom($source)
-    {
-        $curl = curl_init($source);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($curl, CURLOPT_USERPWD, env('API_AUTH_USER') . ":" . env('API_AUTH_PASS'));
-
-        $data = curl_exec($curl);
-        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        if (!$data || $status_code >= 400) {
-            return [];
-        }
-
-        $response = json_decode($data, true);
-
-        $this->meta = $response['meta'] ?? [];
-
-        return $response['data'];
     }
 }
