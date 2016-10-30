2d7ee912435494f651b9636cc9891b4576d79590
brnbp <bruno9pereira@gmail.com>
2016-10-30 14:30:56 -0200

    unify podcasts exibition

--- resources/views/home/logged.blade.php
+++ resources/views/home/logged.blade.php
@@ -7,32 +7,7 @@
                 <section class="scrollable padder-lg" id="bjax-target">
                     <h2 class="font-thin m-b" id="home-title">{{$title}}</h2>
                     <div class="row row-sm podcasts-episodes-home">
-
-                        @foreach($content as $podcast)
-                            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                                <div class="item">
-                                    <div class="pos-rlt">
-                                        <div class="bottom">
-                                            <span class="badge bg-info m-l-sm m-b-sm">{{$podcast['total_episodes']}} Episodes</span>
-                                        </div>
-                                        <div class="item-overlay opacity r r-2x bg-black">
-                                            <div class="center text-center m-t-n">
-                                                <a href="/podcast/{{$podcast['id']}}">
-                                                    <i class="icon-action-redo i-2x"></i>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <a href="#">
-                                            <img src="{{$podcast['thumbnail_600']}}" class="r r-2x img-full">
-                                        </a>
-                                    </div>
-                                    <div class="padder-v">
-                                        <a href="/podcast/{{$podcast['id']}}" class="text-ellipsis">{{$podcast['name']}}</a>
-                                        <a href="/podcast/{{$podcast['id']}}" class="text-ellipsis text-xs text-muted"></a>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
+                        @include('partials.podcasts')
                     </div>
                     <div id="loading" hidden>
                         <div class="row">
---
+++ resources/views/partials/podcasts.blade.php
@@ -0,0 +1,25 @@
+@foreach($content as $podcast)
+    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+        <div class="item">
+            <div class="pos-rlt">
+                <div class="bottom">
+                    <span class="badge bg-info m-l-sm m-b-sm">{{$podcast['total_episodes']}} Episodes</span>
+                </div>
+                <div class="item-overlay opacity r r-2x bg-black">
+                    <div class="center text-center m-t-n">
+                        <a href="/podcast/{{$podcast['id']}}">
+                            <i class="icon-action-redo i-2x"></i>
+                        </a>
+                    </div>
+                </div>
+                <a href="#">
+                    <img src="{{$podcast['thumbnail_600']}}" class="r r-2x img-full">
+                </a>
+            </div>
+            <div class="padder-v">
+                <a href="/podcast/{{$podcast['id']}}" class="text-ellipsis">{{$podcast['name']}}</a>
+                <a href="/podcast/{{$podcast['id']}}" class="text-ellipsis text-xs text-muted"></a>
+            </div>
+        </div>
+    </div>
+@endforeach
