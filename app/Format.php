7b8da7624d9b273fb5b41a2a90faba2588e8008f
brnbp <bruno9pereira@gmail.com>
2016-10-30 02:47:22 -0200

    move format methods to trait

--- app/Format.php
+++ app/Format.php
@@ -1,6 +1,8 @@
 <?php
 namespace App;
 
+use Illuminate\Support\Collection;
+
 
 trait Format
 {
@@ -13,5 +15,43 @@ trait Format
     {
         $separators = ['-', '/', '|'];
         return explode('-', str_replace($separators, '-', $podcastName))[0];
+    }
+
+    private function formatEpisodes(Collection $episodes)
+    {
+        return $episodes->map(function($episode) {
+            return [
+                'id' => $episode['id'],
+                'podcast_id' => $episode['feed_id'],
+                'title' => strip_tags($episode['title']),
+                'link' => $episode['link'],
+                'published_date' => $this->formatData($episode['published_date']),
+                'content' => $episode['content'],
+                'summary' => $episode['summary'] ?? '',
+                'image' => $episode['image'],
+                'duration' => $episode['duration'],
+                'media_url' => $episode['media_url'],
+                'media_type' => $episode['media_type'],
+            ];
+
+        });
+    }
+
+    private function formatPodcasts(Collection $podcasts)
+    {
+        return $podcasts->map(function($podcast) {
+            return [
+                "id" => $podcast['id'],
+                "name" => $this->formatPodcastName($podcast['name']),
+                "url" => $podcast['url'],
+                "thumbnail_30" => $podcast['thumbnail_30'],
+                "thumbnail_60" => $podcast['thumbnail_60'],
+                "thumbnail_100" => $podcast['thumbnail_100'],
+                "thumbnail_600" => $podcast['thumbnail_600'],
+                "total_episodes" => $podcast['total_episodes'],
+                "listeners" => $podcast['listeners'],
+                "last_episode_at" => $this->formatData($podcast['last_episode_at'])
+            ];
+        });
     }
 }
--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Format;
 use App\Podty\Podcasts;
 use App\Podty\UserPodcasts;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 
 class PodcastController extends Controller
@@ -101,48 +100,6 @@ class PodcastController extends Controller
             'podcast'  => array_first($this->getPodcastById($podcastId)),
             'episodes' => $this->formatEpisodes($episodes),
         ]);
-    }
-
-    /**
-     * @param array $episodes
-     * @return array
-     */
-    private function formatEpisodes($episodes)
-    {
-        return $episodes->map(function($episode) {
-            return [
-                'id' => $episode['id'],
-                'podcast_id' => $episode['feed_id'],
-                'title' => strip_tags($episode['title']),
-                'link' => $episode['link'],
-                'published_date' => $this->formatData($episode['published_date']),
-                'content' => $episode['content'],
-                'summary' => $episode['summary'] ?? '',
-                'image' => $episode['image'],
-                'duration' => $episode['duration'],
-                'media_url' => $episode['media_url'],
-                'media_type' => $episode['media_type'],
-            ];
-
-        });
-    }
-
-    private function formatPodcasts(Collection $podcasts)
-    {
-        return $podcasts->map(function($podcast) {
-            return [
-                "id" => $podcast['id'],
-                "name" => $this->formatPodcastName($podcast['name']),
-                "url" => $podcast['url'],
-                "thumbnail_30" => $podcast['thumbnail_30'],
-                "thumbnail_60" => $podcast['thumbnail_60'],
-                "thumbnail_100" => $podcast['thumbnail_100'],
-                "thumbnail_600" => $podcast['thumbnail_600'],
-                "total_episodes" => $podcast['total_episodes'],
-                "listeners" => $podcast['listeners'],
-                "last_episode_at" => $this->formatData($podcast['last_episode_at'])
-            ];
-        });
     }
 
     public function discover()
