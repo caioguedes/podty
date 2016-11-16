667776e498ebe806fdb13cb6c23938fe535d275f
Bruno Pereira <bruno9pereira@gmail.com>
2016-11-16 14:53:54 -0200

    remove date from episode page

--- resources/views/episode.blade.php
+++ resources/views/episode.blade.php
@@ -13,7 +13,7 @@
                                 <div class="
                                         col-lg-4 col-lg-offset-4
                                         col-md-4 col-md-offset-4
-                                        col-sm-4 col-sm-offset-4
+                                        col-sm-6 col-sm-offset-4
                                         ">
                                     <h2>
                                         <a href="/podcast/{{$podcast['slug']}}">{{$podcast['name']}}</a>
@@ -21,15 +21,12 @@
 
                                     <img src="{{$podcast['episodes']['image'] ?: $podcast['thumbnail_600']}}" alt="">
                                     <hr>
+                                    <h3>{{$podcast['episodes']['title']}}</h3>
+                                    <br>
                                     <audio controls id="player" style="width: 100%">
                                         <source src="{{$podcast['episodes']['media_url']}}" id="source">
                                     </audio>
 
-                                    <h2>{{$podcast['episodes']['title']}}</h2>
-                                    <h3>
-                                        {{(new DateTime($podcast['episodes']['published_at']))
-                                                ->format('d/m/Y H:i:s')}}
-                                    </h3>
                                 </div>
                             </div>
                         </section>
