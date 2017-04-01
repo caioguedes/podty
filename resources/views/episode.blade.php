f0bd4b1b18341d7551bf0f121d04f4f2efd1e8eb
brnbp <bruno9pereira@gmail.com>
2017-04-01 18:34:29 -0300

    fix: increase space to layout

--- resources/views/episode.blade.php
+++ resources/views/episode.blade.php
@@ -12,7 +12,7 @@
                         <section class="scrollable">
                             <div class="text-center">
                                 <div class="
-                                        col-lg-4 col-lg-offset-4
+                                        col-lg-6 col-lg-offset-3
                                         col-md-4 col-md-offset-4
                                         col-sm-6 col-sm-offset-4
                                         ">
