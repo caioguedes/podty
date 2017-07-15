742e2ab00776ef490de4654068eb8d5d86869138
brnbp <bruno9pereira@gmail.com>
2017-07-15 13:27:20 -0300

    extract episode html to partial blade file

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -17,73 +17,11 @@
                                   <h2 class="font-thin m-b" id="home-title">Favorites Episodes <small>({{$favorites->count()}})</small></h2>
                                   <div class="row row-sm podcasts-episodes-home">
 
-                                      @foreach($favorites as $favorite)
-                                          <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-                                              <div class="item">
-                                                  <div class="pos-rlt">
-                                                      <div class="bottom">
-                                                          @if($favorite['episode']['duration'])
-                                                              <span class="badge bg-info m-l-sm m-b-sm">
-                                                                  {{$favorite['episode']['duration']}}
-                                                              </span>
-                                                          @endif
-                                                      </div>
-
-                                                      <div class="item-overlay opacity r r-2x bg-black">
-                                                          <a href="#" class="center text-center play-me m-t-n active" data-toggle="class">
-                                                              <input type="hidden"
-                                                                     value="{{$favorite['episode']['media_url']}}"
-                                                                     data-title="{{$favorite['episode']['title']}}"
-                                                                     data-id="{{$favorite['episode']['id']}}"
-                                                                     data-image="{{$favorite['episode']['image']}}"
-                                                              >
-                                                              <i class="icon-control-play text-active i-2x"></i>
-                                                              <i class="icon-control-pause text i-2x"></i>
-                                                          </a>
-
-                                                          <div class="top m-r-sm m-t-sm">
-                                                              <a href="#" class="pull-right btn-unfav-ep" data-toggle="class">
-                                                                  <i class="fa fa-heart-o text-active"></i>
-                                                                  <i class="fa fa-heart text text-danger"></i>
-                                                              </a>
-                                                          </div>
-                                                          <div class="bottom">
-                                                              <a href="/episodes/{{$favorite['episode']['id']}}" class="pull-right m-r-sm m-b-sm" target="_blank">
-                                                                  <i class="icon-action-redo"></i>
-                                                              </a>
-                                                          </div>
-                                                      </div>
-                                                      <a href="#">
-                                                          <img src="{{$favorite['episode']['image'] ?: $favorite['feed']['thumbnail_100']}}" class="r r-2x img-full">
-                                                      </a>
-                                                  </div>
-                                                  <div class="padder-v">
-                                                      <a href="#" class="text-ellipsis" data-toggle="modal" data-target="#myModal{{$favorite['episode']['id']}}">{{$favorite['episode']['title']}}</a>
-                                                      <a href="#" class="text-ellipsis text-xs text-muted" data-toggle="modal" data-target="#myModal{{$favorite['episode']['id']}}">
-                                                          {{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $favorite['episode']['published_date'])->diffForHumans()}}
-                                                      </a>
-                                                      <a href="/podcasts/{{$favorite['feed']['slug']}}" class="text-ellipsis">{{$favorite['feed']['name']}}</a>
-                                                  </div>
-
-                                                  <div class="modal fade" id="myModal{{$favorite['episode']['id']}}" role="dialog">
-                                                      <div class="modal-dialog">
-                                                          <div class="modal-content bg-dark">
-                                                              <div class="modal-body" style="overflow: scroll; max-height: 300px;">
-                                                                  <h4 class="modal-title">{{$favorite['episode']['title']}}</h4>
-                                                                  <hr>
-                                                                  <?= !empty($favorite['episode']['content']) ? $favorite['episode']['content'] : $favorite['episode']['summary']?>
-                                                              </div>
-                                                              <div class="modal-footer">
-                                                                  <button type="button" class="btn btn-info btn-rounded" data-dismiss="modal">
-                                                                      Close
-                                                                  </button>
-                                                              </div>
-                                                          </div>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      @endforeach
+                                      @include('partials.episodes', [
+                                          'episodes' => $favorites,
+                                          'favorite' => true,
+                                      ])
+                                      
                                   </div>
                               </section>
                               <div id="div-player"
---
+++ resources/views/partials/episodes.blade.php
@@ -0,0 +1,75 @@
+@foreach($episodes as $episode)
+    <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
+        <div class="item">
+            <div class="pos-rlt">
+                <div class="bottom">
+                    @if($episode['episode']['duration'])
+                        <span class="badge bg-info m-l-sm m-b-sm">
+                                                  {{$episode['episode']['duration']}}
+                                                                  </span>
+                    @endif
+                </div>
+                
+                <div class="item-overlay opacity r r-2x bg-black">
+                    <a href="#" class="center text-center play-me m-t-n active" data-toggle="class">
+                        <input type="hidden"
+                               value="{{$episode['episode']['media_url']}}"
+                               data-title="{{$episode['episode']['title']}}"
+                               data-id="{{$episode['episode']['id']}}"
+                               data-image="{{$episode['episode']['image']}}"
+                               data-paused-at="{{$episode['episode']['paused_at'] ?? 0}}"
+                        >
+                        <i class="icon-control-play text-active i-2x"></i>
+                        <i class="icon-control-pause text i-2x"></i>
+                    </a>
+                    
+                    <div class="top m-r-sm m-t-sm">
+                        @if(isset($favorite))
+                            <a href="#" class="pull-right btn-unfav-ep" data-toggle="class">
+                                <i class="fa fa-heart-o text-active"></i>
+                                <i class="fa fa-heart text text-danger"></i>
+                            </a>
+                        @else
+                            <a href="#" class="pull-right btn-fav-ep" data-toggle="class">
+                                <i class="fa fa-heart-o text"></i>
+                                <i class="fa fa-heart text-active text-danger"></i>
+                            </a>
+                        @endif
+                    </div>
+                    <div class="bottom">
+                        <a href="/episodes/{{$episode['episode']['id']}}" class="pull-right m-r-sm m-b-sm" target="_blank">
+                            <i class="icon-action-redo"></i>
+                        </a>
+                    </div>
+                </div>
+                <a href="#">
+                    <img src="{{$episode['episode']['image'] ?: $episode['thumbnail_100']}}" class="r r-2x img-full">
+                </a>
+            </div>
+            <div class="padder-v">
+                <a href="#" class="text-ellipsis" data-toggle="modal" data-target="#myModal{{$episode['episode']['id']}}">{{$episode['episode']['title']}}</a>
+                <a href="#" class="text-ellipsis text-xs text-muted" data-toggle="modal" data-target="#myModal{{$episode['episode']['id']}}">
+                    {{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $episode['episode']['published_at'] ?? $episode['episode']['published_date'])->diffForHumans()}}
+                </a>
+                <a href="/podcasts/{{$episode['slug'] ?? $episode['feed']['slug']}}" class="text-ellipsis">{{$episode['name'] ?? $episode['feed']['name']}}</a>
+            </div>
+            
+            <div class="modal fade" id="myModal{{$episode['episode']['id']}}" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content bg-dark">
+                        <div class="modal-body" style="overflow: scroll; max-height: 300px;">
+                            <h4 class="modal-title">{{$episode['episode']['title']}}</h4>
+                            <hr>
+                            <?= !empty($episode['episode']['content']) ? $episode['episode']['content'] : $episode['episode']['summary']?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-info btn-rounded" data-dismiss="modal">
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
